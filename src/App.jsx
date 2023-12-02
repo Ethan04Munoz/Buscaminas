784792773aed386487052d1e8eefc225e229bbd4
Your Name <you@example.com>
2023-12-01 20:23:50 -0600

    posibilidad

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,22 @@ function App() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+  const tableroSize = 10;
+
   const [ubicacionesMinas, setUbicacionesMinas] = useState([]);
+
+  function estaEnAreaProhibida(x, y, eX, eY) {
+    return x >= eX - 1 && x <= eX + 1 && y >= eY - 1 && y <= eY + 1;
+  }
 
   function generarMinasAleatorias(eX, eY) {
     let nuevasUbicacionesMinas = [];
     while (nuevasUbicacionesMinas.length < 10) {
         let x = obtenerNumeroAleatorioEntre(0, 9);
         let y = obtenerNumeroAleatorioEntre(0, 9);
-        if ((x !== eX || y !== eY) && !nuevasUbicacionesMinas.some(mina => mina.x === x && mina.y === y)) {
-            nuevasUbicacionesMinas.push({ x, y });
-        }
+        if (!estaEnAreaProhibida(x, y, eX, eY) && !nuevasUbicacionesMinas.some(mina => mina.x === x && mina.y === y)) {
+          nuevasUbicacionesMinas.push({ x, y });
+      }
     }
     setUbicacionesMinas(nuevasUbicacionesMinas);
   }
@@ -58,17 +64,130 @@ function App() {
   const [primerClic, setPrimerClic] = useState(true);
   const [casillasReveladas, setCasillasReveladas] = useState(new Set());
   const [casillasMarcadas, setCasillasMarcadas] = useState(new Set());
+
+  const calcularMinasVecinas = (x, y) => {
+    let contadorMinas = 0;
+  
+    // Recorrer todas las casillas vecinas
+    for (let dx = -1; dx <= 1; dx++) {
+      for (let dy = -1; dy <= 1; dy++) {
+        if (dx === 0 && dy === 0) continue; // Saltar la casilla actual
+        const newX = x + dx;
+        const newY = y + dy;
+  
+        // Verificar si las nuevas coordenadas están dentro del tablero
+        if (newX >= 0 && newX < tableroSize && newY >= 0 && newY < tableroSize) {
+          if (ubicacionesMinas.some(mina => mina.x === newX && mina.y === newY)) {
+            contadorMinas++;
+          }
+        }
+      }
+    }
+  
+    return contadorMinas;
+  };
+
+  const revelarCasillas = (x, y) => {
+    let contador = 0;
+  
+    // Función para revelar una casilla individual
+    const revelarCasilla = (x, y) => {
+      const clave = `${x}-${y}`;
+      if (casillasReveladas.has(clave) || ubicacionesMinas.some(mina => mina.x === x && mina.y === y)) {
+        return; // Si la casilla ya está revelada o es una bomba, no hacer nada
+      }
+  
+      casillasReveladas.add(clave);
+      contador++;
+    };
+  
+    // Revelar la casilla clickeada
+    revelarCasilla(x, y);
+  
+    // Si no hay minas alrededor de la casilla clickeada, revelar las casillas adyacentes
+    if (calcularMinasVecinas(x, y) === 0) {
+      for (let dx = -1; dx <= 1; dx++) {
+        for (let dy = -1; dy <= 1; dy++) {
+          if (dx === 0 && dy === 0) continue; // Saltar la casilla actual
+          const newX = x + dx;
+          const newY = y + dy;
+  
+          if (newX >= 0 && newX < tableroSize && newY >= 0 && newY < tableroSize) {
+            revelarCasilla(newX, newY);
+          }
+  
+          if (contador >= 10) break; // Detener si se han revelado 10 casillas
+        }
+        if (contador >= 10) break;
+      }
+    }
+  
+    setCasillasReveladas(new Set(casillasReveladas));
+  };
+  
+  function volado(){
+    let numero = Math.round(Math.random(0,2));
+    return numero;
+  }
+
+  function arreglosIguales(arr1, arr2) {
+    // Paso 1: Verificar si los arreglos tienen la misma longitud
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    // Paso 2: Ordenar ambos arreglos
+    arr1.sort();
+    arr2.sort();
+
+    // Paso 3: Comparar los elementos de los arreglos
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
   const manejarClicCasilla = (x, y) => {
     if (primerClic) {
       generarMinasAleatorias(x, y);
       setPrimerClic(false);
+      revelarCasillas(x, y);
     }
-    setCasillasReveladas(new Set(casillasReveladas).add(`${x}-${y}`));
+    if(bombaRevelada == false){
+      if(volado()==1){
+        revelarCasillas(x, y);
+      }else{
+        setCasillasReveladas(new Set(casillasReveladas).add(`${x}-${y}`));
+      }
+    }
+    if(Array.from(casillasMarcadas).length == 10){
+
+    }
   };
+
+  const [bombaRevelada, setBombaRevelada] = useState(false);
+
+  useEffect(() => {
+    casillasReveladas.forEach(casilla => {
+      const [x, y] = casilla.split('-').map(Number);
+      if (ubicacionesMinas.some(mina => mina.x === x && mina.y === y)) {
+        setTimeout(() => {
+          alert("Perdiste!");
+          setBombaRevelada(true);
+        }, 0);
+      }
+    });
+  }, [casillasReveladas, ubicacionesMinas]);
+  
 
   const manejarClicDerecho = (e, x, y) => {
     e.preventDefault();
-    setCasillasMarcadas(new Set(casillasMarcadas).add(`${x}-${y}`));
+    if(bombaRevelada == false){
+      setCasillasMarcadas(new Set(casillasMarcadas).add(`${x}-${y}`));
+    }
   }
 
   function Casilla({ x, y, esMina, numeroMinas }) {
@@ -95,7 +214,6 @@ function App() {
     );
   }
 
-  const tableroSize = 10;
   const contadorMinas = contarMinasAlrededor(tableroSize, ubicacionesMinas);
 
   return (
