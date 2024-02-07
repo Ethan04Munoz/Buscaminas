607ff0ee53d16cc9a98f81f1656127864f28ec0c
Your Name <you@example.com>
2024-02-06 22:01:36 -0600

    Creacion de 2 tipos de petalo

--- src/Flor.jsx
+++ src/Flor.jsx
@@ -4,21 +4,27 @@ import React from 'react';
 function Flor() {
     // Lista de clases para los colores de los pétalos
     const coloresPetalo = ['petalo-rosa', 'petalo-azul', 'petalo-rojo', 'petalo-violeta', 'petalo-tinto'];
-
+    const tiposPetalo = ['petalo-redondo', 'petalo-ovalo10'];
     // Función para obtener un color aleatorio de la lista
     const getColorAleatorio = () => {
         const indiceAleatorio = Math.floor(Math.random() * coloresPetalo.length);
         return coloresPetalo[indiceAleatorio];
     };
+
+    const getTipoPetaloAleatorio = () => {
+        const indiceAleatorio = Math.floor(Math.random() * tiposPetalo.length);
+        return tiposPetalo[indiceAleatorio];
+    }
     const colorFlor = getColorAleatorio();
+    const tipoPetalo = getTipoPetaloAleatorio();
     return (
         <div className="flower">
             {/* Aplicar el color aleatorio a cada pétalo */}
-            <div className={`petal ${colorFlor}`}></div>
-            <div className={`petal ${colorFlor}`}></div>
-            <div className={`petal ${colorFlor}`}></div>
-            <div className={`petal ${colorFlor}`}></div>
-            <div className={`petal ${colorFlor}`}></div>
+            <div className={`petal ${tipoPetalo} ${colorFlor}`}></div>
+            <div className={`petal ${tipoPetalo} ${colorFlor}`}></div>
+            <div className={`petal ${tipoPetalo} ${colorFlor}`}></div>
+            <div className={`petal ${tipoPetalo} ${colorFlor}`}></div>
+            <div className={`petal ${tipoPetalo} ${colorFlor}`}></div>
             <div className="center"></div>
         </div>
     )
