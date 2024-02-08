4015381bafa0d6c3a3128c8f906515bf3274f9bd
Your Name <you@example.com>
2024-02-07 22:22:42 -0600

    centros de la flor del mismo color que petalos

--- src/Flor.jsx
+++ src/Flor.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Flor() {
     // Lista de clases para los colores de los pétalos
     const coloresPetalo = ['petalo-rosa', 'petalo-azul', 'petalo-rojo', 'petalo-violeta', 'petalo-tinto'];
-    const tiposPetalo = ['petalo-redondo', 'petalo-ovalo10'];
+    const tiposPetalo = ['petalo-redondo', 'petalo-ovalo10', 'petalo-pentagono'];
     // Función para obtener un color aleatorio de la lista
     const getColorAleatorio = () => {
         const indiceAleatorio = Math.floor(Math.random() * coloresPetalo.length);
@@ -17,6 +17,23 @@ function Flor() {
     }
     const colorFlor = getColorAleatorio();
     const tipoPetalo = getTipoPetaloAleatorio();
+
+    const getClaseCentro = (colorPetalo) => {
+        // Suponiendo que tienes clases adicionales definidas en tu CSS
+        // que son versiones más oscuras de tus colores de pétalos
+        const centroColorMap = {
+            'petalo-rosa': 'centro-rosa',
+            'petalo-azul': 'centro-azul',
+            'petalo-rojo': 'centro-rojo',
+            'petalo-violeta': 'centro-violeta',
+            'petalo-tinto': 'centro-tinto',
+        };
+
+        return centroColorMap[colorPetalo] || 'centro-default';
+    };
+
+    const claseCentro = getClaseCentro(colorFlor);
+
     return (
         <div className="flower">
             {/* Generar una cantidad aleatoria de petalos entre 2 y 10 */}
@@ -25,7 +42,7 @@ function Flor() {
             <div className={`petal ${tipoPetalo} ${colorFlor}`}></div>
             <div className={`petal ${tipoPetalo} ${colorFlor}`}></div>
             <div className={`petal ${tipoPetalo} ${colorFlor}`}></div>
-            <div className="center"></div>
+            <div className={`center ${claseCentro}`}></div>
         </div>
     )
 }
