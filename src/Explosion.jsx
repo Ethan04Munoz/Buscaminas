41dcfe7c2fc0f569c4110e033bdecaf918b9c962
Your Name <you@example.com>
2024-02-09 20:42:53 -0600

    cenizas

--- src/Explosion.jsx
+++ src/Explosion.jsx
@@ -28,6 +28,15 @@ function Explosion(){
             <div class="parte"></div>
             <div class="parte"></div>
             <div class="parte"></div>
+            <div class="ember" id="ember1"></div>
+            <div class="ember" id="ember2"></div>
+            <div class="ember" id="ember3"></div>
+            <div class="ember" id="ember4"></div>
+            <div class="ember" id="ember5"></div>
+            <div class="ember" id="ember6"></div>
+            <div class="ember" id="ember7"></div>
+            <div class="ember" id="ember8"></div>
+            <div class="ember" id="ember9"></div>
         </div>
     )
 }
