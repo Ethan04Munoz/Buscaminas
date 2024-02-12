accf746a79696678f8d06ed00119fb50499b7d7f
Your Name <you@example.com>
2024-02-12 11:02:22 -0600

    Home incluye el submenu

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,15 @@
 import Navbar from "./Navbar";
 import './Home.css';
+import Submenu from "./Submenu";
 
 function Home(){
     return (
         <div className="home">
             <Navbar/>
+            <Submenu/>
+            <div className='centrarHijos'>
+                
+            </div>
         </div>
     )
 }
