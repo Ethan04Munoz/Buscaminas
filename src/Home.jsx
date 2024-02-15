9dccbf43eca15420d39186f52a7db80a41219168
Your Name <you@example.com>
2024-02-14 21:45:06 -0600

    Ultimo commit 14/02/2024

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,12 +4,20 @@ import Submenu from "./Submenu";
 import { Link } from 'react-router-dom';
 import './App.css'
 import Flor from "./Flor";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Suspense, lazy } from 'react';
 const LazyImage = React.lazy(() => import('./LazyImagen.jsx')); // Asume que tienes un componente LazyImage
 
 function Home(){
-  
+    const [esMovil, setEsMovil] = useState(false);
+    useEffect(() => {
+        console.log("Width heigth: ", window.innerHeight, window.innerWidth)
+        if(window.innerWidth < window.innerHeight){
+            setEsMovil(true);
+        }else{
+          setEsMovil(false);
+        }
+    }, [])
     return (
         <div className="home">
             <Navbar/>
@@ -31,14 +39,16 @@ function Home(){
                     </Suspense>
                 </div>  
                 </Link>
-                <Link to="/hard">
-                <div className="contenedorNivel" id="parteDer">
-                    <p> Dificil </p>
-                    <Suspense fallback={<div>Cargando...</div>}>
-                        <LazyImage src="buscaminasFlor (1).png" alt="" />
-                    </Suspense>
-                </div>
-                </Link>
+                {esMovil==false && (
+                    <Link to="/hard">
+                    <div className="contenedorNivel" id="parteDer">
+                        <p> Dificil </p>
+                        <Suspense fallback={<div>Cargando...</div>}>
+                            <LazyImage src="buscaminasFlor (1).png" alt="" />
+                        </Suspense>
+                    </div>
+                    </Link>
+                )}
             </div>
         </div>
     )
