f118a02cf3897d3046d0036592fff111a270f53a
Your Name <you@example.com>
2024-02-14 21:28:44 -0600

    en moviles no muestra el buscaminas dificil

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,7 +1,15 @@
 import { Link } from 'react-router-dom';
 import './Submenu.css';
+import { useEffect, useState } from 'react';
 
 function Submenu(){
+    const [esMovil, setEsMovil] = useState(false);
+    useEffect(() => {
+        console.log("Width heigth: ", window.innerHeight, window.innerWidth)
+        if(window.innerWidth < window.innerHeight){
+            setEsMovil(true);
+        }
+    }, [])
     return (
         <div className="submenu">
             <div className='contenedorEnlace'>
@@ -10,9 +18,12 @@ function Submenu(){
             <div className='contenedorEnlace'>
                 <Link className="enlaceSubmenu" to="/medium">Buscaminas medio</Link>
             </div>
+            {esMovil == false && (
             <div className='contenedorEnlace'>
                 <Link className="enlaceSubmenu" to="/hard">Buscaminas dificil</Link>
             </div>
+            )}
+
             <div className='contenedorEnlace'>
                 <Link className="enlaceSubmenu" to="/help">Ayuda?</Link>
             </div>
