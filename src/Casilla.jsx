904266b29739eb6597a4c1310c005d5199bd0727
Your Name <you@example.com>
2024-02-16 18:26:54 -0600

    Cambios en casilla para permitir marcar las bombas con clic prolongado

--- src/Casilla.jsx
+++ src/Casilla.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Flor from './Flor';
 import Explosion from './Explosion';
 
@@ -6,6 +6,13 @@ function Casilla({ tamaño, x, y, esMina, numeroMinas, revelada, marcada, maneja
   let clases = tamaño;
   let contenido = "";
   let tonoClase = (x + y) % 2 === 0 ? "colorClaro" : "colorOscuro"; // Clase para el tono
+
+  // Nuevo estado para manejar el inicio del toque
+  const [toqueInicio, setToqueInicio] = useState(0);
+
+  // Constante para la duración mínima del toque (en milisegundos) para considerarlo como toque prolongado
+  const duracionToqueProlongado = 500; // 0.5 segundos
+
   if (estadoJuego === "ganado" && !revelada) {
     contenido = <Flor/>; // Mostrar el componente Flor
   } else if (marcada) {
@@ -16,7 +23,6 @@ function Casilla({ tamaño, x, y, esMina, numeroMinas, revelada, marcada, maneja
       if (estadoJuego === "perdido" && esMina) {
         contenido = <>{contenido}<Explosion/></>;
       }
-      
     } else {
       contenido = numeroMinas > 0 ? numeroMinas : "";
     }
@@ -26,11 +32,31 @@ function Casilla({ tamaño, x, y, esMina, numeroMinas, revelada, marcada, maneja
 
   clases += ` ${tonoClase}`; // Agregar la clase de tono
 
+  // Manejadores para el toque prolongado
+  const manejarInicioToque = (e) => {
+    e.preventDefault(); // Prevenir el comportamiento por defecto del toque
+    setToqueInicio(Date.now()); // Registrar el inicio del toque
+  };
+
+  const manejarFinToque = (e, x, y) => {
+    e.preventDefault(); // Prevenir el comportamiento por defecto al finalizar el toque
+    const duracion = Date.now() - toqueInicio; // Calcular la duración del toque
+    if (duracion >= duracionToqueProlongado) {
+      // Si la duración supera el umbral, se considera toque prolongado
+      manejarClicDerecho(e, x, y);
+    } else {
+      // Si no, se maneja como un toque normal
+      manejarClicCasilla(x, y);
+    }
+  };
+
   return (
     <div
       className={clases}
       onClick={() => manejarClicCasilla(x, y)}
       onContextMenu={(e) => manejarClicDerecho(e, x, y)}
+      onTouchStart={manejarInicioToque}
+      onTouchEnd={(e) => manejarFinToque(e, x, y)}
     >
       {contenido}
     </div>
