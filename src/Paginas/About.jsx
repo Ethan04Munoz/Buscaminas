08eaa83b9dc99e8a70f86fac5fb510a277d79128
Your Name <you@example.com>
2024-06-05 21:12:59 -0600

    Añadido de los creditos en la música

--- src/Paginas/About.jsx
+++ src/Paginas/About.jsx
@@ -38,6 +38,12 @@ function AboutPage(){
                     <li>
                         {translations[language].aboutCreditosMusicaPor} <a href="https://pixabay.com/users/lemonmusicstudio-14942887/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=108600">Vitaliy Levkin</a> {translations[language].aboutCreditosMusicaDe} <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=108600">Pixabay</a>
                     </li>
+                    <li>
+                        {translations[language].aboutCreditosMusicaPor} <a href="https://www.youtube.com/@Chillpeach">Chillpeach</a> {translations[language].aboutCreditosMusicaDe} <a href="https://www.youtube.com/watch?v=1t1Rp9Nx--M&list=PLQwRmTwWx0ubcpv0SkHroupj1LqW5_Va5&index=7">Youtube</a>
+                    </li>
+                    <li>
+                        {translations[language].aboutCreditosMusicaPor} <a href="https://www.youtube.com/@BreakingCopyright">BreakingCopyright — Royalty Free Music</a> {translations[language].aboutCreditosMusicaDe} <a href="https://www.youtube.com/watch?v=3TKghKSDnEM&list=PLfP6i5T0-DkIMnLY0r17o_kOOsVtQIJIX&index=2">Youtube</a>
+                    </li>
                 </ul>
             </div>
             <div className="espacio"></div>
